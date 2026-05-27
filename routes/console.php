186c85e82867e016a4400c52fd7dfd82b5e95f77
Thobiq <malyasyithobiq@gmail.com>
2026-05-27 22:46:49 +0700

    fixing backup feature

--- routes/console.php
+++ routes/console.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-// $schedule->command('backup:clean')->dailyAt('01:00');
-// $schedule->command('backup:run --only-db')->dailyAt('01:30');
+Schedule::command('backup:clean')->dailyAt('01:00');
+Schedule::command('backup:run --only-db')->dailyAt('01:30');
